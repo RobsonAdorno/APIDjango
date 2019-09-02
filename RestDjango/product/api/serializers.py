1ff3a0db282367c6ade81ba6bf3a5b51858be48a
Robson Adorno <rferreira@scopus.com.br>
2019-09-02 14:58:54 -0300

    [CommitBomba] Fazendo o viewset

--- RestDjango/product/api/serializers.py
+++ RestDjango/product/api/serializers.py
@@ -1,3 +1,9 @@
+from rest_framework import serializers
+from RestDjango.product.models import Product
 
-class ProductSerializer:
-    pass
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
